eb00445ffa5241d88b575cc271fe9e9101a414e5
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-09-17 11:00:13 +0200

    Added Testmethods

--- MathTest/UnitTest1.cs
+++ MathTest/UnitTest1.cs
@@ -12,13 +12,19 @@ namespace MathTest
         private MyMath m = new MyMath();
 
         [TestMethod]
-        public void Add()
+        public void AddPLus()
         {
             Assert.IsTrue(m.Add(2,3).Equals(5));
         }
 
         [TestMethod]
-        public void Negative()
+        public void AddPlusMinus()
+        {
+            Assert.IsTrue(m.Add(-2, 3).Equals(1));
+        }
+
+        [TestMethod]
+        public void AddNegative()
         {
             Assert.IsTrue(m.Add(-2, -3).Equals(-5));
         }
@@ -30,9 +36,21 @@ namespace MathTest
         }
 
         [TestMethod]
+        public void DivideNegative()
+        {
+            Assert.IsTrue(m.Divide(-10, -5).Equals(2));
+        }
+
+        [TestMethod]
         public void Multiply()
         {
             Assert.IsTrue(m.Multiply(10, 5).Equals(50));
+        }
+
+        [TestMethod]
+        public void MultiplyNegative()
+        {
+            Assert.IsTrue(m.Multiply(-10, 5).Equals(-50));
         }
     }
 }
