ab0bcd3a4e0872b1340140d77c0b153d417b11ac
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-09-17 11:49:38 +0200

    added basic summeries to MyMathNew class

--- MyMathNew/MyMath.cs
+++ MyMathNew/MyMath.cs
@@ -11,26 +11,61 @@ namespace MyMathNew
     /// </summary>
     public class MyMath
     {
+        /// <summary>
+        /// plus two int'ergers together
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int Add(int a, int b)
         {
             return a + b;
         }
 
+        /// <summary>
+        /// plus Three int'ergers together
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
         public int Add(int a, int b, int c)
         {
             return a + b + c;
         }
 
+
+        /// <summary>
+        /// minus two int'ergers together
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int Substract(int a, int b)
         {
             return a - b;
         }
 
+
+        /// <summary>
+        /// devides two int'ergers together
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int Divide(int a, int b)
         {
             return a / b;
         }
 
+
+
+        /// <summary>
+        /// Multiplies two int'ergers together
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public int Multiply(int a, int b)
         {
             return a * b;
